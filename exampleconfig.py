bea24687d390b85e27a96d4b995617f18161090d
rmm2020 <rachel.m.molaro@gmail.com>
2021-03-29 17:12:31 -0700

    added files

---
+++ exampleconfig.py
@@ -0,0 +1,5 @@
+sender_email = "coolengineer@gmail.com"
+sender_password = "hunter2"
+receiver_email = "cooltechnician@gmail.com"
+slack_token = 'FLK2039580238540923850982305923FAKETOKEN'
+slack_channel_name = "equipment-operators"
